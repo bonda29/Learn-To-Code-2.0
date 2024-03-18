46327940a714afd9ec054c9bafabfe2410b32ca4
bonda29 <bondableglobe10@gmail.com>
2024-03-18 11:32:27 +0200

    small changes

--- src/main/java/com/example/learntocode/services/messages/MessageReceiver.java
+++ src/main/java/com/example/learntocode/services/messages/MessageReceiver.java
@@ -5,15 +5,18 @@ import com.example.learntocode.models.DirectMessage;
 import com.example.learntocode.payload.DTOs.DirectMessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MessageReceiver {
+    private final SimpMessagingTemplate template;
 
 
     @RabbitListener(queues = RabbitMQConfig.directMessagesQueue)
     public void receiveDirectMessage(DirectMessageDto message) {
         System.out.println("Received message: " + message);
+        template.convertAndSend("/topic/messages", message);
     }
 }
